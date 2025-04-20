0000bc12b3afacc70139345b49c8f15adfca01c6
Ilenol <lonely931103@gmail.com>
2025-04-20 20:31:14 +0800

    add 01257103

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,6 +2,7 @@ public class FirstClass4Team {
   public static void main(String[] args) {
   System.out.println("這是用來練習GitHub協作的程式!");
   System.out.println("\"學號：\"00957056\"\n姓名：\"姜竣維\"");
-  System.out.println("\"學號：\"01257110\"\n姓名：\"林師麒\"")
+  System.out.println("\"學號：\"01257110\"\n姓名：\"林師麒\"");
+  System.out.println("\"學號：\"01257103\"\n姓名：\"林彥均\"");
   }
 }
--- Second_FirstClass4Team.java
+++ Second_FirstClass4Team.java
@@ -3,6 +3,6 @@ public class FirstClass4Team {
          System.out.println("這是用來練習GitHub協作的程式!");
          System.out.println("\"學號：\"00957056\"\n姓名：\"姜竣維\"");        
          System.out.println("\"學號：\"01257110\"\n姓名：\"林師麒\"");   
-         System.out.println("\"學號：\"23456789\"\n姓名：\"申山本\"");   
+         System.out.println("\"學號：\"23456789\"\n姓名：\"林彥均\"");   
      }
  }
