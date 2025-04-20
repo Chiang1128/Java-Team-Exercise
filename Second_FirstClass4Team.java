29c9d5ac8d47f708d68e2a2f2298e8df18a16b21
Chiang1128 <124015003+Chiang1128@users.noreply.github.com>
2025-04-20 20:14:36 +0800

    Create Second_FirstClass4Team.java

---
+++ Second_FirstClass4Team.java
@@ -0,0 +1,9 @@
+public class FirstClass4Team {
+     public static void main(String[] args) {
+         System.out.println("這是用來練習GitHub協作的程式!");
+         System.out.println("\"學號：\"00957056\"\n姓名：\"姜竣維\"");        
+         System.out.println("\"學號：\"12345678\"\n姓名：\"谷翔平\"");   
+         System.out.println("\"學號：\"23456789\"\n姓名：\"申山本\"");   
+     }
+ }
+ }
