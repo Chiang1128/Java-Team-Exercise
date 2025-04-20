7a4d43b673d2853affd748a442b38afda0dfe41f
ProASQ <a40802bf@gmail.com>
2025-04-20 20:18:53 +0800

    Update FirstClass4Team.java

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,5 +2,6 @@ public class FirstClass4Team {
   public static void main(String[] args) {
   System.out.println("這是用來練習GitHub協作的程式!");
   System.out.println("\"學號：\"00957056\"\n姓名：\"姜竣維\"");
+  System.out.println("\"學號：\"01257110\"\n姓名：\"林師麒\"")
   }
 }
